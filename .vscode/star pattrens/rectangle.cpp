d59aa9c7e8a23aa0b4dd613fe01a6ca525f38bdc
moamnhmdany <moamnhmdany2@gmail.com>
2024-08-30 22:59:20 +0300

    rect star

---
+++ .vscode/star pattrens/rectangle.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j, hight, width;
+    cout << "Enter rectangle hight  ";
+    cin >> hight;
+    cout << "Enter rectangle width  ";
+    cin >> width;
+
+    for (i = 1; i <= hight; i++)
+    {
+
+        if (i == 1 || i == hight)
+        {
+            for (j = 1; j <= width; j++)
+            {
+                cout << "*";
+            }
+        }
+        else
+        {
+            for (j = 1; j <= width; j++)
+            {
+                if (j == 1 || j == width)
+                {
+                    cout << "*";
+                }
+                else
+                {
+                    cout << " ";
+                }
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
